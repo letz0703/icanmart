5131a5528aa2d0aa56ad42bdef5ad88a6dfc259a
letz0703 <letz0703@gmail.com>
2019-08-04 01:56:13 +0900

    REDIS: boxes/index.bla bone

--- resources/views/boxes/index.blade.php
+++ resources/views/boxes/index.blade.php
@@ -2,50 +2,31 @@
 
 @section('content')
     <boxes-view inline-template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Boxes</div>
-                    <div class="card-body">
-                        @foreach($boxes as $box)
-                            <box :data="{{ $box }}" v-cloak>
-                            {{--<div class="level">--}}
-                                {{--<div class="flex">--}}
-                                    {{--<a href="{{ $box->path() }}">--}}
-                                        {{--{{ $box->arrived_at }}--}}
-                                        {{--{{ $box->seller->name }}:--}}
-                                    {{--</a>--}}
-                                    {{--@can('update')--}}
-                                    {{--<span v-if="paid">--}}
-                                        {{--<button class="btn btn-primary btn-sm" @click="unpaid">paid</button>--}}
-                                    {{--</span>--}}
-                                    {{--<span v-else>--}}
-                                            {{--<button class="btn btn-danger btn-sm" @click="make">unpaid</button>--}}
-                                    {{--</span>--}}
-                                    {{--@endcan--}}
-                                        {{--{{ $box->title }} [--}}
-                                    {{--@if ($box->paid)--}}
-                                        {{--paid--}}
-                                    {{--@else--}}
-                                        {{--<span style="color:red;">unpaid</span>--}}
-                                    {{--@endif--}}
-                                    {{--]--}}
-                                {{--</div>--}}
-                                {{--<div>--}}
-                                    {{--{{ $box->items_count }}--}}
-                                    {{--{{ str_plural('item',$box->items_count) }}--}}
-                                    {{--[ total: {{ $box->amount }} 원 ]--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
-                            </box>
-                            <hr>
-                        @endforeach
-                        {{ $boxes->links() }}
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Boxes</div>
+                        <div class="card-body">
+                            @foreach($boxes as $box)
+                                <box :data="{{ $box }}" v-cloak></box>
+                                <hr>
+                            @endforeach
+                            {{ $boxes->links() }}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Recent View
+                        </div>
+                        <div class="card-body">
+                            Redis Thread here
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     </boxes-view>
 @endsection
