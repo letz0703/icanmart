91833241c5e8ea2fae4349b0336994de2832109e
letz0703 <letz0703@gmail.com>
2019-10-15 03:44:06 +0900

    search.blade

--- resources/views/items/search.blade.php
+++ resources/views/items/search.blade.php
@@ -3,7 +3,23 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-{{--                <app></app>--}}
+                <div class="card-body">
+                    <form method="get" action="/items/search">
+                        <div class="form-group">
+                            <input type="text"
+                                   class="form-control"
+                                   name="query"
+                                   placeholder="Search for Item"
+                                   autofocus="true"
+                            >
+                            <button type="submit" class="btn btn-outline-info btn-sm">
+                                Search
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+                {{--                <app></app>--}}
                 <div class="card">
                     <div class="card-header">
                         Items
@@ -11,9 +27,13 @@
                     <div class="card-body">
                         <ul class="list-group">
                             @foreach($items as $item)
-{{--                                <a href="{{ url($item->path) }}">--}}
-                                    <li class="list-group-item">{{ $item->product_name }}</li>
-{{--                                </a>--}}
+                                {{--                                <a href="{{ url($item->path) }}">--}}
+                                <li class="list-group-item">
+                                    <a href="{{ url($item->path()) }}">
+                                        {{ $item->product_name }} {{ $item->sell_price }}[ <sup>{{ $item->buy_price }}</sup> ]
+                                    </a>
+                                </li>
+                                {{--                                </a>--}}
                             @endforeach
                         </ul>
                     </div>
