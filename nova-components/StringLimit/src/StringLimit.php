14a68da564d33f27d7cc883e711956010f797743
letz <letz0703@gmail.com>
2019-12-18 19:14:28 +0900

    nova:field

---
+++ nova-components/StringLimit/src/StringLimit.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Icanmart\StringLimit;
+
+use Laravel\Nova\Fields\Field;
+
+class StringLimit extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'string-limit';
+}
