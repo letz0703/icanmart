e398058e52d2b8d813dfc2c91c1a542d1453de5b
letz <letz0703@gmail.com>
2019-12-19 20:22:53 +0900

    Nova: Custom Theme

---
+++ nova-components/NovaTheme/src/ThemeServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Icanmart\NovaTheme;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::theme(asset('/icanmart/nova-theme/theme.css'));
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('icanmart/nova-theme'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
