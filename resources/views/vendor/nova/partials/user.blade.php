abdb725ee312a3e3322131be9d47ea731de018f8
letz <letz0703@gmail.com>
2019-12-19 20:06:37 +0900

    Nova: customer link to admin dashboard

--- resources/views/vendor/nova/partials/user.blade.php
+++ resources/views/vendor/nova/partials/user.blade.php
@@ -14,6 +14,9 @@
 <dropdown-menu slot="menu" width="200" direction="rtl">
     <ul class="list-reset">
         <li>
+            <a href="{{ route('admin.dashboard.index') }}" class="block no-underline text-90 hover:bg-30 p-3">
+                Admin Dashboard
+            </a>
             <a href="{{ route('nova.logout') }}" class="block no-underline text-90 hover:bg-30 p-3">
                 {{ __('Logout') }}
             </a>
