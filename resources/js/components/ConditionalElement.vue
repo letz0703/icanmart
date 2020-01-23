8987f011096a7c9ccca367a0897b994119e05b64
letz <letz0703@gmail.com>
2020-01-23 21:21:00 +0900

    style <transition>

--- resources/js/components/ConditionalElement.vue
+++ resources/js/components/ConditionalElement.vue
@@ -1,28 +1,44 @@
 <template>
-    <div v-show="isVisible">
-        <slot></slot>
-    </div>
+    <transition name="fade">
+        <div v-show="isVisible">
+            <slot></slot>
+        </div>
+    </transition>
 </template>
 
 <script>
     import inVue from 'in-viewport';
+
     export default {
         props: ['whenHidden'],
 
         mounted() {
-            window.addEventListener('scroll', this.checkDisplay, { passive: true});
+            window.addEventListener('scroll', this.checkDisplay, { passive: true });
         },
 
         data() {
             return {
                 isVisible: false;
-            }
+        }
         },
 
         methods: {
-            checkDisplay(){
-                this.Visible = ! inVue(document.querySelector(this.whenHidden));
-            }
+            checkDisplay() {
+                this.Visible = !inVue(document.querySelector(this.whenHidden));
+            },
         },
     }
 </script>
+
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+        /*transition: all 3s;*/
+        transition: fade .3s;
+    }
+
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
+    }
+</style>
