a162c3141b4be84de58c5d4f5f5d00bc06006e84
letz <letz0703@gmail.com>
2020-01-23 19:53:50 +0900

    tool : <conditional-emelement>

---
+++ resources/js/components/ConditionalElement.vue
@@ -0,0 +1,28 @@
+<template>
+    <div v-show="isVisible">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import inVue from 'in-viewport';
+    export default {
+        props: ['whenHidden'],
+
+        mounted() {
+            window.addEventListener('scroll', this.checkDisplay, { passive: true});
+        },
+
+        data() {
+            return {
+                isVisible: false;
+            }
+        },
+
+        methods: {
+            checkDisplay(){
+                this.Visible = ! inVue(document.querySelector(this.whenHidden));
+            }
+        },
+    }
+</script>
