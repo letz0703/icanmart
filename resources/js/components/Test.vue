15c6fbc2c6cd8e03c962cb5887c8ee102bc64880
letz <letz0703@gmail.com>
2020-01-31 19:27:56 +0900

    testing vue

---
+++ resources/js/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    let data= { message: 'hello'};
+    new Vue({
+        el: '#root',
+        data: data
+    });
+
+    export default {
+        props: [''],
+
+        data() {
+            return {
+                message: '안녕'
+            }
+        }
+    }
+</script>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,10 +1,23 @@
-@extends('layouts.app')
-@section('content')
-    <div class="flex justify-center items-center h-screen">
-        <div class="flex ">
-            <p>Hello There
-                <span data-tooltip="I am a tool tip">Hover over me</span>
-            </p>
-        </div>
-    </div>
-@endsection
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="kr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div id="root">
+    <input type="text" id="input" v-model="message">
+    <p> Message is : @{{ message }}</p>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+
+<script>
+    let data = { message: 'hello' };
+    new Vue({
+        el: '#root',
+        data: data
+    });
+</script>
+</body>
+</html>
