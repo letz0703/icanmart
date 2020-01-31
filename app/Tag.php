d4fbbf591212b5e23d00f4951909964145889237
letz <letz0703@gmail.com>
2020-01-31 21:28:04 +0900

    pivot table for item tag

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)->withTimestamps();
+    }
+
 }
---
+++ database/migrations/2020_01_31_202853_create_item_tag_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_tag', function (Blueprint $table) {
+            $table->primary(['item_id','tag_id']);
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('tag_id');
+
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_tag');
+    }
+}
