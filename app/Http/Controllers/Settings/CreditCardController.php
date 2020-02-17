220ef542e41375ac6d883b20b1d0296555a74fc6
letz <letz0703@gmail.com>
2020-02-17 20:30:10 +0900

    laravel6 confirm password

---
+++ app/Http/Controllers/Settings/CreditCardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Settings;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreditCardController extends Controller
+{
+    public function edit()
+    {
+        return 'show info';
+    }
+}
