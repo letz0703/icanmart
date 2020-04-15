ed8845795d856eff670847af2eb519c4268f765d
letz <letz0703@gmail.com>
2020-04-16 04:54:49 +0900

    UI: fix New Project Button margin right

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <header class="mb-6">
-        <div class="flex justify-between items-baseline">
-            <h2 class="">My Projects</h2>
-            <a href="/projects/create" class="button">New Project</a>
+    <header class="flex items-center mb-3 py-2">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="font-normal">My Projects</h2>
+            <a href="/projects/create" class="button" style="">New Project</a>
         </div>
     </header>
-    <div class="flex flex-wrap -mx-3">
-        @forelse($projects as $project)
+    <main class="lg:flex flex-wrap -mx-3">
+        @forelse ($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
                 @include('projects.card')
             </div>
         @empty
-            <p>No Project yet</p>
+            <div>No Projects Yet</div>
         @endforelse
-    </div>
+    </main>
 @endsection
