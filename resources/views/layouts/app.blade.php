e7db1f5f72864dd20357c052a47e68eaeb5d1ed7
letz <letz0703@gmail.com>
2020-05-13 18:56:10 +0900

    main css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
+    <link href="{{ asset('css/icon-font.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/ic.css') }}" rel="stylesheet">
     <style>
--- resources/views/welcome/202004.blade.php
+++ resources/views/welcome/202004.blade.php
@@ -19,7 +19,44 @@
             <a href="" class='button'>구매요청</a>
         </div>
     </div>
-    {{--    @include('layouts.grid')--}}
+    {{--        @include('layouts.grid')--}}
+
+    <section class="section-features">
+        <div class="row">
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-heart"></i>
+                    <h3 class="heading-tertiary u-margin-bottom-small">인기 상품</h3>
+                    <p class="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+                        esse.</p>
+                </div>
+            </div>
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-magnifier"></i>
+                    <h3 class="heading-tertiary u-margin-bottom-small">아이템 검색</h3>
+                    <p class="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+                        esse.</p>
+                </div>
+            </div>
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-star"></i>
+                    <h3 class="heading-tertiary u-margin-bottom-small">추천 상품</h3>
+                    <p class="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+                        esse.</p>
+                </div>
+            </div>
+            <div class="col-1-of-4">
+                <div class="feature-box">
+                    <i class="feature-box__icon icon-basic-message-multiple"></i>
+                    <h3 class="heading-tertiary u-margin-bottom-small">정보 공유</h3>
+                    <p class="feature-box__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+                        esse.</p>
+                </div>
+            </div>
+        </div>
+    </section>
     <div class="main__menu">
         <div class="main__menu-card">
             <a href="/items/search">
