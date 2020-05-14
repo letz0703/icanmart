da89846a348265ea8f35fb7865006b1c0cfbe6c6
letz <letz0703@gmail.com>
2020-05-14 21:33:42 +0900

    popular items part-1

--- resources/views/welcome/202004.blade.php
+++ resources/views/welcome/202004.blade.php
@@ -3,14 +3,14 @@
         <div class="flex justify-between">
             <div class="card__menu-title mb-3">
                 <h2>인터넷 깡통시장</h2>
-                <p class="text-grey">부산 깡통시장 구매대행</p>
+                <p class="text-grey">부산 깡통시장 상품정보</p>
             </div>
             <div class="card__menu-menu">
                 <nav class="py-2">
                     <ul class="navigation">
                         <li><a href="#" class="btn-text">About Us</a></li>
                         <li><a href="#" class="btn-text">OnlineShop</a></li>
-                        <li><a href="#" class="btn-text">Contact</a></li>
+                        {{--                        <li><a href="#" class="btn-text">Contact</a></li>--}}
                     </ul>
                 </nav>
             </div>
@@ -62,13 +62,35 @@
             </div>
         </div>
     </section>
-    <div class="main__menu">
-        <div class="main__menu-card">
-            <a href="/items/search">
-                아이템 검색
-            </a>
+    <section class="section-items">
+        <div class="text-center u-margin-bottom-big">
+            <h2 class="heading-secondary uppercase">Popular Items</h2>
+            <div class="row">
+                <div class="col-1-of-3">
+                    <div class="item-card">
+                        <div class="item-card__side">
+                            동전파스
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1-of-3">
+                    <div class="item-card">
+                        <div class="item-card__side">
+                            곤약젤리
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1-of-3">
+                    <div class="item-card">
+                        <div class="item-card__side">
+                            맥심커피
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="main__menu-card">오늘의 상품</div>
+    </section>
+    <div class="main__menu">
         @if (auth()->user() && auth()->user()->isAdmin)
             <div class="main__menu-card">
                 <h2>관리</h2>
