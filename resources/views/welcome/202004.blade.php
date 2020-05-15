7607b681cfd38967f63f4f48f157d5426afb2d68
letz <letz0703@gmail.com>
2020-05-16 08:36:07 +0900

    rotate card back side color

--- resources/views/welcome/202004.blade.php
+++ resources/views/welcome/202004.blade.php
@@ -3,13 +3,13 @@
         <div class="flex justify-between">
             <div class="card__menu-title mb-3">
                 <h2>인터넷 깡통시장</h2>
-                <p class="text-grey">부산 깡통시장 상품정보</p>
+                <p class="text-grey">부산 깡통시장 아이템 정보</p>
             </div>
             <div class="card__menu-menu">
                 <nav class="py-2">
                     <ul class="navigation">
                         <li><a href="#" class="btn-text">About Us</a></li>
-                        <li><a href="#" class="btn-text">OnlineShop</a></li>
+                        <li><a href="#" class="btn-text">Member Shop</a></li>
                         {{--                        <li><a href="#" class="btn-text">Contact</a></li>--}}
                     </ul>
                 </nav>
@@ -67,11 +67,11 @@
             <h2 class="heading-secondary uppercase">Popular Items</h2>
             <div class="row">
                 <div class="col-1-of-3">
-                    <div class="item-card">
-                        <div class="item-card__side item-card__side--front">
+                    <div class="rotate-card">
+                        <div class="rotate-card__side rotate-card__side--front">
                             동전파스
                         </div>
-                        <div class="item-card__side item-card__side--back">
+                        <div class="rotate-card__side rotate-card__side--back rotate-card__side--back-1">
                             동전파스
                         </div>
 
