280a2f3d5e56010155a7410b92f90c532d4f6aab
letz <letz0703@gmail.com>
2020-05-26 18:49:12 +0900

    fix gravatar link

--- app/helpers.php
+++ app/helpers.php
@@ -4,8 +4,8 @@ function avatar_url($email){
     $email = md5($email);
     //return "https://avatar.com/{$email}?s=60&d='https://s3.amazoneaws.com/laracasts/images/default-square-avatar.jpg'";
 
-    return  "https://avatar.com/{$email}".http_build_query([
+    return  "https://avatar.com/{$email}?".http_build_query([
             's' => 60,
-            'd' => 'https://s3.amazoneaws.com/laracasts/images/default-square-avatar.jpg'
+            'd' => 'https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg'
         ]);
 }
