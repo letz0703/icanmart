62a477cc673c484e8a5ced02a623cd70fd89e38e
letz <letz0703@gmail.com>
2020-06-18 21:23:40 +0900

    UI: welcome202004 product

--- resources/views/welcome/202004.blade.php
+++ resources/views/welcome/202004.blade.php
@@ -32,12 +32,13 @@
             <div class="row mb-10">
                 <div class="col-1-of-3">
                     <div class="rotate-card">
-                        <div class="rotate-card__side rotate-card__side--front rotate-card__side--front-1">
+                        <div class="rotate-card__side rotate-card__side--front rotate-card__side--front--1">
                             <div class="rotate-card__picture rotate-card__picture--1">
                                 &nbsp;
                             </div>
                             <h4 class="rotate-card__heading">
-                                <span class="rotate-card__heading-span rotate-card__heading-span--1 px-2 py-1">Pain Relief Patches
+                                <span class="rotate-card__heading-span rotate-card__heading-span--1 px-2 py-1">
+                                    Pain Relief Patches
                                 </span>
                             </h4>
                             <div class="rotate-card__detail">
@@ -47,14 +48,18 @@
                                     <li>500원 크기-78매</li>
                                 </ul>
                             </div>
-
-
                         </div>
                         <div class="rotate-card__side rotate-card__side--back rotate-card__side--back--1">
+                            <div class="rotate-card__cta">
 
-                            <div class="rotate-card__heading pt-2">동전파스 9800원</div>
-                            <div class="rotate-card__detail text-xl -mt-1">
-                                <a href="https://www.roihi.com/en/tubokou.html" target="_blank">homepage</a>
+                                <div class="rotate-card__price-box md:mt-6">
+                                    <h4 class="rotate-card__item-name">동전파스</h4>
+                                    <div class="rotate-card__price-value">9800원</div>
+                                </div>
+                                <div class="rotate-card__detail text-xl -mt-1">
+                                    <a href="https://www.roihi.com/en/tubokou.html" target="_blank">homepage</a>
+                                    <a href="#" class="btn btn--white">shop now!</a>
+                                </div>
                             </div>
                         </div>
 
@@ -62,46 +67,95 @@
                 </div>
                 <div class="col-1-of-3">
                     <div class="rotate-card">
-                        <div class="rotate-card__side rotate-card__side--front">
-                            <div class="rotate-card__heading p-2">추천 아이템</div>
-                            <div class="rotate-card__heading pt-2 mb-2">용각산 사탕</div>
-                            <div class="rotate-card__detail text-xl -mt-1">
-                                목이 따갑거나 아플 때
+                        <div class="rotate-card__side rotate-card__side--front rotate-card__side--front--2">
+                            <div class="rotate-card__picture rotate-card__picture--2">
+                                &nbsp;
                             </div>
-                            <p class="font-normal md:text-base">한알이면 끝</p>
+                            <h4 class="rotate-card__heading">
+                                <span class="rotate-card__heading-span rotate-card__heading-span--2 px-2 py-1">
+                                   Cabagin Alpha 300
+                                </span>
+                            </h4>
+                            <div class="rotate-card__detail">
+                                <ul>
+                                    <li>Free Shipping</li>
+                                    <li>No.1 Japanese item</li>
+                                    <li></li>
+                                </ul>
+                            </div>
                         </div>
-                        <div class="rotate-card__side rotate-card__side--back rotate-card__side--back--1">
-{{--                            <div class="rotate-card__picture rotate-card__picture--1">--}}
-{{--                                &nbsp;--}}
+                        <div class="rotate-card__side rotate-card__side--back rotate-card__side--back--2">
+                            <div class="rotate-card__cta">
+
+                                <div class="rotate-card__price-box md:mt-6">
+                                    <h4 class="rotate-card__item-name">캬베진 알파 300정</h4>
+                                    <div class="rotate-card__price-value">28,000원</div>
+                                </div>
+                                <div class="rotate-card__detail text-xl -mt-1">
+                                    <a href="https://hc.kowa.co.jp/cabagin/" target="_blank">homepage</a>
+                                    <a href="#" class="btn btn--white">shop now!</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-1-of-3">
+                    <div class="rotate-card">
+                        <div class="rotate-card__side rotate-card__side--front rotate-card__side--front--2">
+                            <div class="rotate-card__picture rotate-card__picture--1">
+                                &nbsp;
+                            </div>
+                            <h4 class="rotate-card__heading">
+                                <span class="rotate-card__heading-span rotate-card__heading-span--1 px-2 py-1">
+                                    Yonggaksan neck candy
+                                </span>
+                            </h4>
+                            <div class="rotate-card__detail">
+                                <ul>
+                                    <li>목이 따가울 때</li>
+                                    <li>목을 많이 사용할 때</li>
+                                    <li>한알이면 OK!</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="rotate-card__side rotate-card__side--back rotate-card__side--back--2">
+                            <div class="rotate-card__cta">
+
+                                <div class="rotate-card__price-box md:mt-6">
+                                    <h4 class="rotate-card__item-name">용각산 사탕 100g</h4>
+                                    <div class="rotate-card__price-value">5500원</div>
+                                </div>
+                                <div class="rotate-card__detail text-xl -mt-1">
+                                    <a href="https://www.ryukakusan.co.jp/promotion5/en" target="_blank">homepage</a>
+                                    <a href="#" class="btn btn--white">shop now!</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+{{--                <div class="col-1-of-3">--}}
+{{--                    <div class="rotate-card">--}}
+{{--                        <div class="rotate-card__side rotate-card__side--front">--}}
+{{--                            <div class="rotate-card__heading p-2">여름 아이템</div>--}}
+{{--                            <div class="rotate-card__heading pt-2 mb-2">블랜디 액상커피</div>--}}
+{{--                            <div class="rotate-card__detail text-xl -mt-1">--}}
+{{--                                아이스 커피용 커피 원액--}}
 {{--                            </div>--}}
-                            <div class="rotate-card__heading pt-2">용각산사탕 5000원</div>
-                            <div class="rotate-card__detail text-xl -mt-1">
-                                <a href="https://www.ryukakusan.co.jp/productcategory/en" target="_blank">homepage</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-1-of-3">
-                    <div class="rotate-card">
-                        <div class="rotate-card__side rotate-card__side--front">
-                            <div class="rotate-card__heading p-2">여름 아이템</div>
-                            <div class="rotate-card__heading pt-2 mb-2">블랜디 액상커피</div>
-                            <div class="rotate-card__detail text-xl -mt-1">
-                                아이스 커피용 커피 원액
-                            </div>
-                            <p class="font-normal md:text-base">간편하고 맛있게 즐기세요</p>
-                        </div>
-                        <div class="rotate-card__side rotate-card__side--back rotate-card__side--back--1">
-                            {{--                            <div class="rotate-card__picture rotate-card__picture--1">--}}
-                            {{--                                &nbsp;--}}
-                            {{--                            </div>--}}
-                            <div class="rotate-card__heading pt-2">블랜디 액상커피 8개입 2700원</div>
-                            <div class="rotate-card__detail text-xl -mt-1">
-                                <a href="https://blendy.agf.jp/portion/" target="_blank">homepage</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+{{--                            <p class="font-normal md:text-base">간편하고 맛있게 즐기세요</p>--}}
+{{--                        </div>--}}
+{{--                        <div class="rotate-card__side rotate-card__side--back rotate-card__side--back--1">--}}
+{{--                            --}}{{--                            <div class="rotate-card__picture rotate-card__picture--1">--}}
+{{--                            --}}{{--                                &nbsp;--}}
+{{--                            --}}{{--                            </div>--}}
+{{--                            <div class="rotate-card__heading pt-2">블랜디 액상커피 8개입 2700원</div>--}}
+{{--                            <div class="rotate-card__detail text-xl -mt-1">--}}
+{{--                                <a href="https://blendy.agf.jp/portion/" target="_blank">homepage</a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
             </div>
         </div>
     </section>
