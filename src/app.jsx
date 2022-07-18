8164ca84624bcd361afb060729e3002c34b0e177
ManI <letz0703@gmail.com>
2022-07-19 03:47:55 +0900

    Main Pass

--- src/app.jsx
+++ src/app.jsx
@@ -5,16 +5,16 @@ import Intro from './components/intro/intro';
 
 function App() {
   const [code, setCode] = useState([]);
-  const icPasses = [{cell: '01037441815'}];
+  const icPasses = [{cell: '1815'}];
 
   return (
-    <div>
-      <Nav />
-      <input type='text' className={styles.intro_input} placeholder='패스 코드를 입력 하세요' onChange={(e) => setCode(e.target.value)} required />
+    <div class={styles.app}>
+      <input type='text' className={styles.intro_input} placeholder='회원번호를 입력 하세요' onChange={(e) => setCode(e.target.value)} required />
       {icPasses.map((icPass) => {
         if (icPass.cell === code) {
           return (
             <>
+              <Nav />
               <Intro />
             </>
           );
