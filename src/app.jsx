e7acb21954450c393996739b639687ec453a553a
ManI <letz0703@gmail.com>
2022-07-19 01:56:59 +0900

    custom scss

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from './app.module.css';
 import Nav from './components/nav/nav';
 import Intro from './components/intro/intro';
 
 function App() {
+  const [code, setCode] = useState([]);
+  const icPasses = [{cell: '01037441815'}];
+
   return (
-    <>
+    <div>
       <Nav />
-      <Intro />
-    </>
+      <input type='text' className={styles.intro_input} placeholder='패스 코드를 입력 하세요' onChange={(e) => setCode(e.target.value)} required />
+      {icPasses.map((icPass) => {
+        if (icPass.cell === code) {
+          return (
+            <>
+              <Intro />
+            </>
+          );
+        }
+      })}
+    </div>
   );
 }
 
